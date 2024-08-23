cba3a2c2be1c7e699654ee2a040e6e21521e1950
ademyavuz42 <ademyavuz@gmail.com>
2024-08-23 21:24:39 +0700

    remove second highest

---
+++ src/main/java/RemoveSecondHighest.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class RemoveSecondHighest {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1,"Apple");
+        map.put(3,"Banana");
+        map.put(5,"Orange");
+        map.put(2,"Grapes");
+        map.put(4,"Mango");
+
+        System.out.println("Original HashMap: " + map);
+
+        removeSecondHighest(map);
+
+        System.out.println("HashMap after removing the second highest key: " + map);
+    }
+
+    private static void removeSecondHighest(HashMap<Integer, String> map) {
+        if (map.size() < 2) {
+            System.out.println("Not enough elements to remove second highest number!");
+            return;
+        }
+        List<Integer> sortedKeys = new ArrayList<>(map.keySet());
+        Collections.sort(sortedKeys, Collections.reverseOrder());
+
+        Integer secondHighestKey = sortedKeys.get(1);
+        map.remove(secondHighestKey);
+    }
+}
