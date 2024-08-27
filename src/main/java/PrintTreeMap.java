8868b7f040f1bd85be9b1dec9a5405d35c4d47b2
ademyavuz42 <ademyavuz@gmail.com>
2024-08-27 12:47:30 +0700

    TreeMap

--- src/main/java/PrintTreeMap.java
+++ src/main/java/PrintTreeMap.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class PrintTreeMap {
+import java.util.Map;
+import java.util.TreeMap;
+
+// A TreeMap stores key-value pairs in a sorted order based on the keys.
+public class PrintTreeMap {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        treeMap.put("Banana",30);
+        treeMap.put("Orange", 40);
+        treeMap.put("Apple", 50);
+        treeMap.put("Mango", 70);
+        System.out.println("TreeMap elements: ");
+        for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
+            System.out.println(entry.getKey() +" : "+  entry.getValue());
+        }
+    }
 }
